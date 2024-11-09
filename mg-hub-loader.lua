891eda746c9ed409f116e0bdf363ad6ab75a1e5a
stfulua <mr_k1ll3r-56BACKUP@proton.me>
2024-11-09 18:00:15 +0100

    Update mg-hub-loader.lua

--- mg-hub-loader.lua
+++ mg-hub-loader.lua
@@ -1,1 +1,9 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/stfulua/MG-Hub/refs/heads/main/games.lua"))()
 
+for PlaceID, Execute in pairs(Games) do
+    if PlaceID == game.PlaceId then
+        print("Executing script for PlaceID: " .. PlaceID)
+        loadstring(game:HttpGet(Execute))()
+        break
+    end
+end
