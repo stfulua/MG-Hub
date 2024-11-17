72e1e1144e87be9e479515e42dcbe40400121784
stfulua <mr_k1ll3r-56BACKUP@proton.me>
2024-11-17 16:05:14 +0100

    Update null.lua

--- games/null.lua
+++ games/null.lua
@@ -1,1 +1,18 @@
-print("Null")
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+Rayfield:Notify({
+   Title = "MG Hub",
+   Content = "Game Not Supported!",
+   Duration = 6.5,
+   Image = 4483362458,
+   Actions = { -- Notification Buttons
+
+      Ignore = { -- Duplicate this table (or remove it) to add and remove buttons to the notification.
+         Name = "Okay!",
+         Callback = function()
+            print("The user tapped Okay!")
+         end
+      },
+},
+Rayfield:Destroy()
+})
