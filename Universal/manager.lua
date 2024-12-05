28e82c6a72500d5ef9f41f6b996748d56fd7c60e
stfulua <mr_k1ll3r-56BACKUP@proton.me>
2024-12-05 22:45:23 +0100

    Update manager.lua

---
+++ Universal/manager.lua
@@ -0,0 +1,8 @@
+local CoreGui = game:GetService("StarterGui")
+
+CoreGui:SetCore("SendNotification", {
+	Title = "Mschub";
+	Text = "Universal Scripts Manager!";
+	Duration = 5;
+	Button1 = "Okay!";
+})
